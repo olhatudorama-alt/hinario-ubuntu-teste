573289b8d228971c1ff5e17c3dce82bfbd844b43
olhatudorama-alt <olhatudorama@gmail.com>
2026-02-22 18:47:59 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,26 @@
-const CACHE = 'hinario-ubuntu-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+{
+  "name": "Hinário Ubuntu",
+  "short_name": "Hinário Ubuntu",
+  "description": "Hinário digital da Igreja Ubuntu com letras e áudios",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#FFC72C",
+  "theme_color": "#FFC72C",
+  "orientation": "portrait",
+  "lang": "pt-BR",
+  "id": "/hinario-ubuntu-teste/",
+  "icons": [
+    {
+      "src": "https://i.imgur.com/4aZvsOD.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any"
+    },
+    {
+      "src": "https://i.imgur.com/4aZvsOD.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any"
+    }
+  ]
+}
